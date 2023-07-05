2556ee67ce63b2f131dd03eea1934d381c5a6c0e
GS-Desilva <86835581+GS-Desilva@users.noreply.github.com>
2023-07-05 14:15:31 +0530

    Creating ActivityAddCar and adding methods

--- app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
@@ -1,5 +1,32 @@
 package com.assesment1.s4669024_s4669021;
 
-public class ActivityAddCar {
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityAddCar extends AppCompatActivity implements View.OnClickListener{
+
+    Button addcar2Btn;
+    EditText addBrandSpace,addModelSpace,addPriceSpace;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcar);
+
+        addcar2Btn=(Button) findViewById(R.id.addcar2btn);
+        addBrandSpace=findViewById(R.id.addBrandSpace);
+        addModelSpace=findViewById(R.id.addModelSpace);
+        addPriceSpace=findViewById(R.id.addPriceSpace);
+
+        addcar2Btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        int id=view.getId();
+    }
 }

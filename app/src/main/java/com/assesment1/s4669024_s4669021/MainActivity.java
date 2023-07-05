770855437f9b07a54fa9ee0fba844d38689602d0
TimashaFernando <timashafernando29@gmail.com>
2023-07-05 11:49:37 +0530

    Adding the tool bar (MainActivity.java)

--- app/src/main/java/com/assesment1/s4669024_s4669021/MainActivity.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/MainActivity.java
@@ -1,6 +1,7 @@
 package com.assesment1.s4669024_s4669021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the Toolbar from the layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        // Set the Toolbar as the ActionBar
+        setSupportActionBar(toolbar);
     }
 }

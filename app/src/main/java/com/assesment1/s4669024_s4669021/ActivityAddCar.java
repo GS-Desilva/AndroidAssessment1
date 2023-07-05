133e27d3fa5df6ec6a1ba7e84d28d4bf1cf69762
GS-Desilva <86835581+GS-Desilva@users.noreply.github.com>
2023-07-05 16:12:12 +0530

    Update ActivityAddCar.java

--- app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
@@ -5,19 +5,23 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.assesment1.s4669024_s4669021.DBHelper;
 public class ActivityAddCar extends AppCompatActivity implements View.OnClickListener{
 
     Button addcar2Btn;
     EditText addBrandSpace,addModelSpace,addPriceSpace;
-
+    DBHelper dbHelper;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcar);
 
+        dbHelper=new DBHelper(this);
+
         addcar2Btn=(Button) findViewById(R.id.addcar2btn);
+
         addBrandSpace=findViewById(R.id.addBrandSpace);
         addModelSpace=findViewById(R.id.addModelSpace);
         addPriceSpace=findViewById(R.id.addPriceSpace);
@@ -38,5 +42,12 @@ public class ActivityAddCar extends AppCompatActivity implements View.OnClickLis
         String model =addModelSpace.getText().toString();
         String price =addPriceSpace.getText().toString();
 
+        boolean isInserted=dbHelper.insertData(brand,model, Integer.parseInt(price));
+        if(isInserted){
+            Toast.makeText(ActivityAddCar.this,"Data Inserted successfully!", Toast.LENGTH_LONG).show();
+
+        }else{
+            Toast.makeText()
+        }
     }
 }

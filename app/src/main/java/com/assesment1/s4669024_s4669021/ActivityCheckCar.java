a9d233adf6fef878a1272c4ff359bd3d780dd5e9
TimashaFernando <timashafernando29@gmail.com>
2023-07-05 15:29:09 +0530

    ActivityCheckCar updated

--- app/src/main/java/com/assesment1/s4669024_s4669021/ActivityCheckCar.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/ActivityCheckCar.java
@@ -1,5 +1,34 @@
 package com.assesment1.s4669024_s4669021;
 
-public class ActivityCheckCar {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class ActivityCheckCar extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnBack, checkPriceBtn;
+    EditText checkBrandEx,checkModelEx,checkPriceEx;
+    DBHelper DBHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkcar);
+
+        //btnBack = (Button) findViewById(R.id.btnBack);
+        checkPriceBtn = (Button) findViewById(R.id.checkPriceBtn);
+
+
+        checkBrandEx = (EditText) findViewById(R.id.checkBrandEx);
+        checkModelEx = (EditText) findViewById(R.id.checkModelEx);
+        checkPriceEx = (EditText) findViewById(R.id.checkPriceEx);
+
+        btnBack.setOnClickListener(this);
+        checkPriceBtn.setOnClickListener(this);
+
+        DBHelper = new DBHelper(this);
+    }
 }

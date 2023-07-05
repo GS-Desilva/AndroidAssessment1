bb554c94544c6508326019bd80aa9181d7ee6f5e
GS-Desilva <86835581+GS-Desilva@users.noreply.github.com>
2023-07-05 16:54:24 +0530

    Updated ActivityAddCar.java after fixing the typo errors

--- app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/ActivityAddCar.java
@@ -1,17 +1,15 @@
 package com.assesment1.s4669024_s4669021;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.assesment1.s4669024_s4669021.DBHelper;
 public class ActivityAddCar extends AppCompatActivity implements View.OnClickListener{
 
-    Button addcar2Btn;
+    Button addCar2Btn;
     EditText addBrandSpace,addModelSpace,addPriceSpace;
     DBHelper dbHelper;
     protected void onCreate(Bundle savedInstanceState){
@@ -20,13 +18,13 @@ public class ActivityAddCar extends AppCompatActivity implements View.OnClickLis
 
         dbHelper=new DBHelper(this);
 
-        addcar2Btn=(Button) findViewById(R.id.addcar2btn);
+        addCar2Btn= findViewById(R.id.addcar2btn);
 
         addBrandSpace=findViewById(R.id.addBrandSpace);
         addModelSpace=findViewById(R.id.addModelSpace);
         addPriceSpace=findViewById(R.id.addPriceSpace);
 
-        addcar2Btn.setOnClickListener(this);
+        addCar2Btn.setOnClickListener(this);
     }
 
     @Override

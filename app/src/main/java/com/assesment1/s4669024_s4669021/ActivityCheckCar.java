3cae4405451caf8b54a95ea69cf0eeb047b856a6
TimashaFernando <timashafernando29@gmail.com>
2023-07-06 08:33:33 +0530

    updated diplayPrice method

--- app/src/main/java/com/assesment1/s4669024_s4669021/ActivityCheckCar.java
+++ app/src/main/java/com/assesment1/s4669024_s4669021/ActivityCheckCar.java
@@ -1,5 +1,6 @@
 package com.assesment1.s4669024_s4669021;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,24 +10,25 @@ import com.assesment1.assessment_s4669024_s4669021.R;
 import androidx.appcompat.app.AppCompatActivity;
 public abstract class ActivityCheckCar extends AppCompatActivity implements View.OnClickListener{
 
-    Button checkPriceBtn;
+    Button checkPriceBtn, backBtn;
     EditText checkBrandEx,checkModelEx,checkPriceEx;
     DBHelper DBHelper;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkcar);
 
         checkPriceBtn = findViewById(R.id.checkPriceBtn);
-
+        backBtn = findViewById(R.id.backBtn);
 
         checkBrandEx = findViewById(R.id.checkBrandEx);
         checkModelEx = findViewById(R.id.checkModelEx);
         checkPriceEx = findViewById(R.id.checkPriceEx);
 
-
         checkPriceBtn.setOnClickListener(this);
+        backBtn.setOnClickListener(this);
 
         DBHelper = new DBHelper(this);
     }
@@ -35,29 +37,42 @@ public abstract class ActivityCheckCar extends AppCompatActivity implements View
     public void onClick(View view) {
         int id = view.getId();
 
-        if(id == R.id.checkPriceBtn) {
-            viewPrice();
+        if(id == R.id.backBtn)
+        {
+            goToHomePage();
+        }
+
+        if(id == R.id.checkPriceBtn)
+        {
+            displayPrice();
         }
     }
 
-    private void viewPrice() {
+    private void displayPrice(){
         String brand = checkBrandEx.getText().toString().trim();
         String model = checkModelEx.getText().toString().trim();
 
-        if(brand.isEmpty() || model.isEmpty()) {
-            Toast.makeText(this, "Please enter brand name and model name!", Toast.LENGTH_SHORT).show();
+        if(brand.isEmpty())
+        {
+            Toast.makeText(this, "Please enter brand name!", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        int price = DBHelper.getPrice(brand, model);
+        if(model.isEmpty())
+        {
+            Toast.makeText(this, "Please enter model name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        if(price == -1) {
-            Toast.makeText(this, "Car not found. Please try again!", Toast.LENGTH_SHORT).show();
+        int price = DBHelper.getPrice(brand,model);
+
+        if(price == -1){
+            Toast.makeText(this, "Car not found. Please enter the brand and model again!",Toast.LENGTH_SHORT).show();
             checkPriceEx.setText("");
         }
         else {
             checkPriceEx.setText(String.valueOf(price));
         }
     }
-
 }
+
